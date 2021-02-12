fe53d62334bd5f69302be41fb43e2ce2cce7d11b
William Truppi <williamt@hotmail.it>
2021-02-12 12:13:38 +0100

    footer implementato

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,2 +1,92 @@
+  <footer>
+    <div class="container d-flex">
+
+      <div class="footer_column">
+        <img src="{{asset("img/logo.png")}}" alt="">
+      </div>
+
+      <div class="footer_column d-flex">
+        <h3 class="my-title-footer">PASTIFICIO</h3>
+        <ul class="my-list-footer">
+          <li><a href="https://www.lamolisana.it/pastificio/">Il Pastificio</a></li>
+          <li><a href="https://www.lamolisana.it/grano-decorticato-a-pietra/">Grano decorticato a pietra</a></li>
+          <li><a href="https://www.lamolisana.it/molise-esiste/">Il Molise c’è</a></li>
+          <li><a href="https://www.lamolisana.it/filiera-integrata/">Filiera Integrata</a></li>
+          <li><a href="https://www.lamolisana.it/cento-anni-di-pasta/">100 anni di pasta</a></li>
+          <li><a href="https://www.lamolisana.it/sartoria-della-pasta/">Sartoria della pasta</a></li>
+          <li><a href="https://www.lamolisana.it/spaghetto-quadrato/">Spaghetto Quadrato</a></li>
+          <li><a href="https://www.lamolisana.it/chi-siamo-quattro-cugini-unico-obiettivo/">Le Persone</a></li>
+        </ul>
+        <br>
+        <h3 class="my-title-footer">PRODOTTI</h3>
+        <ul class="my-list-footer">
+          <li><a href="https://www.lamolisana.it/le-classiche/">Le Classiche</a></li>
+          <li><a href="https://www.lamolisana.it/le-integrali/">Le Integrali</a></li>
+          <li><a href="https://www.lamolisana.it/le-speciali/">Le Speciali</a></li>
+          <li><a href="https://www.lamolisana.it/le-biologiche/">Le Biologiche</a></li>
+          <li><a href="https://www.lamolisana.it/gluten-free/">Le Gluten-Free</a></li>
+          <li><a href="https://www.lamolisana.it/le-semole/">Le Semole</a></li>
+          <li><a href="https://www.lamolisana.it/extra-di-lusso/">Le Extra di Lusso</a></li>
+        </ul>
+      </div>
+
+      <div class="footer_column d-flex">
+        <h3 class="my-title-footer">COLLEZIONE DA CHEF</h3>
+        <ul class="my-list-footer">
+          <li><a href="https://www.lamolisana.it/collezione-da-chef/">Collezione da Chef</a></li>
+          <li><a href="https://www.lamolisana.it/grandi-cucine/">Grandi Cucine</a></li>
+          <li><a href="https://www.lamolisana.it/biologiche/">Biologiche</a></li>
+          <li><a href="https://www.lamolisana.it/le-quadrate/">Quadrate</a></li>
+        </ul>
+        <br>
+        <a class="my-title-footer" href="https://www.lamolisana.it/limited-edition/"><h3>LIMITED EDITION</h3></a>
+        <br>
+        <a class="my-title-footer" href="https://www.lamolisana.it/notizie/"><h3>NEWS</h3></a>
+        <br>
+        <a class="my-list-footer" href="https://goo.gl/GPgvbc/" target="_blank" rel="noopener noreferrer">» E-COMMERCE</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/b2bdocs/" target="_blank" rel="noopener noreferrer">» B2B</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/comunicazione/">» COMUNICAZIONE</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/scarica-i-cataloghi/">» SCARICA I CATALOGHI</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/rassegna-stampa/">» RASSEGNA STAMPA</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/video/">» VIDEO</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/contatti/">» CONTATTI</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/lavora-con-noi/">» LAVORA CON NOI</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/certificazioni/">» CERTIFICAZIONI</a>
+        <a class="my-list-footer" href="https://www.lamolisana.it/privacy-policy/">» PRIVACY POLICY</a>
+      </div>
+
+      <div class="footer_column">
+        <div class="info_box">
+          <p class="my-footer-par">
+            <strong>Ragione sociale:</strong> La Molisana S.P.A.
+            <br>
+            <strong>Sede legale:</strong> Contrada Colle delle Api, 100/A - 86100 - Campobasso (CB)
+            <br>
+            <strong>Pec:</strong> lamolisana@pec.it
+            <br>
+            <strong>Tel:</strong> +39 0874 4981
+            <br>
+            <strong>Fax:</strong> +39 0874 629584
+            <br>
+            Per segnalazioni scrivere a:
+            <br>
+            <strong><u><a href="mailto:info@lamolisana.it">info@lamolisana.it</a></u></strong>
+            <br>
+            Per contattare l'area commerciale:
+            <br>
+            <strong><u><a href="mailto:commerciale@lamolisana.it">commerciale@lamolisana.it</a></u></strong>
+            <br>
+            Per contattare l'area export:
+            <br>
+            <strong><u><a href="mailto:export@lamolisana.it">export@lamolisana.it</a></u></strong>
+            <br>
+          	Dati aziendali: 
+            <strong><u><a href="https://www.lamolisana.it/dati-aziendali">leggi di più</a></u></strong></p>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+  
   </body>
 </html>
